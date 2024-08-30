7dd9fa3f20a8c6df37a629b4d98ed927daad83d1
sunwongkim <sunwongkim0609@gmail.com>
2024-08-30 23:47:22 +0900

    Add Stage

--- Assets/3_Script/Player.cs
+++ Assets/3_Script/Player.cs
@@ -22,23 +22,21 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         ani = GetComponent<Animator>();
-        playerLayer = LayerMask.NameToLayer("Player");
-        enemyLayer = LayerMask.NameToLayer("Enemy");
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R)) // R: Player 위치 초기화
-            transform.position = new Vector2(0, 0);
-
+            resetPosition();
+            
         if (Input.GetButtonDown("Jump") && !ani.GetBool("isJump")){
             rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             ani.SetBool("isJump", true);
         }
-
+        
         if (Input.GetButton("Horizontal")) // 바라보는 방향
             sr.flipX = Input.GetAxis("Horizontal") < 0;
-    
+        
         ani.SetBool("isRun", Mathf.Abs(rb.velocity.x) >= 2);
     }
 
@@ -46,16 +44,16 @@ public class Player : MonoBehaviour
     {
         float moveInput = Input.GetAxis("Horizontal"); // Move Speed
         rb.AddForce(Vector2.right * moveInput * moveSpeed, ForceMode2D.Impulse);
-
+        
         if (Mathf.Abs(rb.velocity.x) > maxSpeed) // Max Speed
             rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
         
         // 바닥 확인 (by Ray)
         if (rb.velocity.y < 0){ // 떨어지는 중
             Debug.DrawRay(rb.position, Vector3.down, new Color(0, 1, 0));
-            RaycastHit2D rayHit = Physics2D.Raycast(rb.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            RaycastHit2D rayHit = Physics2D.Raycast(rb.position, Vector3.down, 2, LayerMask.GetMask("Platform"));
             // 바닥 확인
-            if (rayHit.collider != null && rayHit.distance < 0.5f)
+            if (rayHit.collider != null && rayHit.distance < 1)
                 ani.SetBool("isJump", false);
         }
     }
@@ -67,10 +65,10 @@ public class Player : MonoBehaviour
             gameManager.stagePoint += 50;
         } else if (other.gameObject.tag == "Finish"){
             gameManager.NextStage();
+            resetPosition();
         } else if (other.gameObject.tag == "GameManager"){ // 추락
             OnDamaged(other.transform.position);
-            // rb.velocity = Vector2.zero;
-            transform.position = new Vector2(0, 0);
+            resetPosition();
         }
     }
 
@@ -94,6 +92,8 @@ public class Player : MonoBehaviour
     void OnDamaged(Vector2 targetPosition)
     {
         // Enemy와 충돌 무시(투명 효과)
+        playerLayer = LayerMask.NameToLayer("Player");
+        enemyLayer = LayerMask.NameToLayer("Enemy");
         Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, true);
         // Graphics
         sr.color = new Color(1, 0, 1, 0.5f);
@@ -115,4 +115,6 @@ public class Player : MonoBehaviour
         Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, false); // 투명 해제
         sr.color = new Color(1, 1, 1, 1);
     }
+
+    void resetPosition() {transform.position = new Vector2(0, 1);}
 }
